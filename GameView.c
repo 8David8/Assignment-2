c83d0802ab293a63d6aae95620ffcd27275dcc7a
z5109929 <z5109929@cse.unsw.EDU.AU>
2016-09-22 22:46:13 +1000

    made major changes to GameView.c

--- GameView.c
+++ GameView.c
@@ -1,61 +1,115 @@
 // GameView.c ... GameView ADT implementation
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include "Globals.h"
 #include "Game.h"
 #include "GameView.h"
 
-#define FIRST_ROUND 1
+#define FIRST_ROUND 1 
+#define NUM_HUNTERS 4
+
+// below defines the number of characters that represents various data
+// about a particular player and his/her move and actions
+// mainly used for the pastPlay string 
+// read the data page for more information about Past Plays
+#define NUM_CHAR_PLAYER 1
+#define NUM_CHAR_NEW_LOCATION 2
+#define NUM_CHAR_ENCOUNTER_HUNTER 4
+#define NUM_CHAR_ENCOUNTER_DRACULA 2
+#define NUM_CHAR_ACTION_DRACULA 1
 
 // #include "Map.h" ... if you decide to use the Map ADT
 
 typedef struct _player {
     int health;
-    int location;
+    // LocationID is typedef'd as an int in the Places.h file 
+    // which is linked via the Game.h file
+    LocationID location; //location of the hunters 
 } player;
 
 struct gameView {
-   player playerStats[NUM_PLAYERS];
-   int score;
-   int turns;
-   int round;
+    player playerStats[NUM_PLAYERS]; // NUM_PLAYERS is defined in Globals.h
+    // the trail array stores dracula last 6 moves
+    // which translate to dracula's last 6 recent location, 
+    // moves = locationID confirmed in the rules page
+    LocationID trail[TRAIL_SIZE]; //TRAIL_SIZE is defined in Game.h
+    int score;
+    int turns;
+    int rounds;  // Can be treated as current round or number of round occurred 
 };
 
+// ----------------------------------------------
+// --- function prototypes that we created :D --- 
+// ----------------------------------------------
 
+static PlayerID convertPlayerNameAbbrevToID(char *abbrev);
 
+// ----------------------------------------------
+
+// IMPORTANT: newGameView gets called once and only once
+// to elaborate it gets called once in HunterView.c and DracView.c
+// GameView is a pointer to the gameView struct, typedef'd in GameView.h
 // Creates a new GameView to summarise the current state of the game
 GameView newGameView(char *pastPlays, PlayerMessage messages[])   {
-    assert(pastPlays != NULL); //Check inputs are valid
-    assert(messages != NULL);  // Check inputs are valid
-    int counter = 0;  
+    assert(pastPlays != NULL); assert(messages != NULL);  // Check parameters are valid
+
+    GameView gView = malloc(sizeof(struct gameView));
+    assert(gView != NULL); // Check if the new gameView is initialised
    
-    if(gameView->round == 0) { // if the turn in the struct = 0 makes a freshhhh game.
-        GameView gameView = malloc(sizeof(struct gameView));
-        gameView->turn = 0;
-        gameView->score = GAME_START_SCORE;
-        while (counter != PLAYER_MINA_HARKER) {
-            gameView->playerStats[counter]->health = GAME_START_HUNTER_LIFE_POINT; // sets all player health to default
-            gameView->playerStats[counter]->location = // i dunno where the player locations should be.
-            counter++;
-        }
-        
-        gameView->playerStats[PLAYER_DRACULA]->health = GAME_START_BLOOD_POINTS; // sets dracs blood points in the beginning
-        gameView->playerStats[PLAYER_DRACULA]->location = // dunno dracs location.
-    
-    } else {
-       disposeGameView(gameView); // calls the dispose functions that will delete stuff. should this new here? because of we dispose of it
-       // how would we access the old stats?
-       GameView gameView = malloc(sizeof(struct gameView));
-       gameView->turn = getScore(gameView);
-       gameView->round = turn/NUM_PLAYERS; // check this
-       while (counter != PLAYER_MINA_HARKER){
-           gameView->playerStat[counter]->health = getHealth(gameView, counter); //grabs the health of player
-           gameView->playerStat[counter]->location = getLocation(gameView counter); // grabs the location of the player
-           counter++;
-       }
+    gView->score = GAME_START_SCORE;
+    gView->turns = (strlen(pastPlays)+1)/8; 
+    gView->round = gView->turns/NUM_PLAYERS;
+
+    // initialise all the hunter players stats
+    int hunterCount;  
+    for (hunterCount = 0; hunterCount < NUM_HUNTERS; hunterCount++){
+        gameView->playerStats[hunterCounter].health = GAME_START_HUNTER_LIFE_POINT; // sets all player health to default
     }
-    return gameView;
+
+    // initialise Dracula's stats
+    gameView->playerStats[PLAYER_DRACULA].health = GAME_START_BLOOD_POINTS; // sets dracs blood points in the beginning
+   
+    // pastPlays is a string containing a history of moves and actions
+    // it represents everything that has happened so far in the game
+    // Each play/turn is represented by 7 characters seperated by a space(another character)
+    // the seven characters indicate what has happened during the player's turn
+    // so to loop through each play/turn we increment index by 8 :D
+    int index;
+    for(index = 0; pastPlays[index] != '\0'; index += 8){
+        // get the name abbrev for the current play and store it in a seperate array
+        char playerNameAbbrev[NUM_CHAR_PLAYER+1];
+        playerNameAbbrev[0] = pastPlays[index]; player[1] = '\0';
+        
+        // get the location abbrev for the current play and store it in a seperate array
+        char newLocation[NUM_CHAR_NEW_LOCATION+1]; 
+        newLocation[0] = pastPlays[index+1]; 
+        newLocation[1] = pastPlays[index+2]; 
+        newLocation[2] = '\0';
+
+        PlayerID currPlayer = convertPlayerNameAbbrevToID(playerNameAbbrev);
+         
+        // current player is Dracula
+        if (currPlayer == PLAYER_DRACULA){
+                
+            //injure or heal Dracula, it gets performed here  
+
+
+        //current player is a hunter   
+        } else {
+             Player currHunter = currPlayer;
+
+             //then check where the hunter is
+             //if at hospital heal him
+             //if he/she encountered Drac or a trap, injured him >:  
+             //if he/she died teleport him to nearest hospital
+             //update current position
+             //you get the point :D
+        }
+    }
+    return gView;
 }
 
 // Frees all memory previously allocated for the GameView toBeDeleted
@@ -69,7 +123,27 @@ void disposeGameView(GameView toBeDeleted)
 }
 
 
-//// Functions to return simple information about the current state of the game
+// ---------------------------------------------------------------------------
+// --- all functions that we created and hence was not in the default file ---
+// ---------------------------------------------------------------------------
+
+static PlayerID convertPlayerNameAbbrevToID(char *abbrev)
+{
+    PlayerID currPlayer; int firstIndex = 0;
+    switch (abbrev[firstIndex]){
+        case 'G': currPlayer = PLAYER_LORD_GODALMING; break;
+        case 'S': currPlayer = PLAYER_DR_SEWARD; break;
+        case 'H': currPlayer = PLAYER_VAN_HELSING; break;
+        case 'M': currPlayer = PLAYER_MINA_HARKER; break;
+        case 'D': currPlayer = PLAYER_DRACULA; break;
+        default: exit(1);
+    }       
+    return currPlayer;
+}
+
+// ----------------------------------------------------------------------------------
+// --- Functions to return simple information about the current state of the game ---
+// ----------------------------------------------------------------------------------
 
 // Get the current round
 Round getRound(GameView currentView)
@@ -108,7 +182,9 @@ LocationID getLocation(GameView currentView, PlayerID player)
     return currentView->playerStats[player].location;
 }
 
-//// Functions that return information about the history of the game
+// -----------------------------------------------------------------------
+// --- Functions that return information about the history of the game ---
+// -----------------------------------------------------------------------
 
 // Fills the trail array with the location ids of the last 6 turns
 void getHistory(GameView currentView, PlayerID player,
@@ -117,10 +193,11 @@ void getHistory(GameView currentView, PlayerID player,
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
 }
 
-//// Functions that query the map to find information about connectivity
+// ---------------------------------------------------------------------------
+// --- Functions that query the map to find information about connectivity ---
+// ---------------------------------------------------------------------------
 
 // Returns an array of LocationIDs for all directly connected locations
-
 LocationID *connectedLocations(GameView currentView, int *numLocations,
                                LocationID from, PlayerID player, Round round,
                                int road, int rail, int sea)
