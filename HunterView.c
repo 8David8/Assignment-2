a50dfbe5361be81c1fd22b00f33e99e268aa472e
z5115759 <z5115759@cse.unsw.EDU.AU>
2016-09-22 16:55:15 +1000

    YOLO8

--- HunterView.c
+++ HunterView.c
@@ -9,17 +9,25 @@
 // #include "Map.h" ... if you decide to use the Map ADT
      
 struct hunterView {
-    player playerStats[NUM_PLAYERS-1]
-    int hello;
+    player playerStats[NUM_PLAYERS-1] // the number of hunters
+    int score;
+    int turn;
+    int round;
 };
      
 
 // Creates a new HunterView to summarise the current state of the game
 HunterView newHunterView(char *pastPlays, PlayerMessage messages[])
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    int counter =0;
     HunterView hunterView = malloc(sizeof(struct hunterView));
-    hunterView->hello = 42;
+    hunterView->score = getScore(gameView); //should just grab it from the most current gameview
+    hunterView->turn = //
+    hunterView->round = getRound(gameView);
+    while(counter != PLAYER_MINA_HARKER) {
+        hunterView->playerStat[counter]->health = getHealth(gameView counter);
+	hunterView->playerStat[counter]->location = getLocation(gameView counter);
+    }
     return hunterView;
 }
      
@@ -29,6 +37,8 @@ void disposeHunterView(HunterView toBeDeleted)
 {
     //COMPLETE THIS IMPLEMENTATION
     free( toBeDeleted );
+    toBeDeleted = NULL;
+    return;
 }
 
 
@@ -37,36 +47,48 @@ void disposeHunterView(HunterView toBeDeleted)
 // Get the current round
 Round giveMeTheRound(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return (currentView->round);
 }
 
 // Get the id of current player
 PlayerID whoAmI(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    PlayerID curr = 0;
+    Round currTurn = getTurn(currView);
+    if (currTurn % NUM_PLAYERS == 1) {
+    	curr = 1;
+    } else if (currTurn % NUM_PLAYERS == 2) {
+	curr = 2
+    } else if (currTurn % NUM_PLAYERS == 3) {
+	curr = 3
+    } else if (currTurn % NUM_PLAYERS == 4) {
+	curr = 4
+    } else {
+	curr = 0;
+    }
+    return curr;
 }
 
 // Get the current score
 int giveMeTheScore(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return currentView->score;
 }
 
 // Get the current health points for a given player
 int howHealthyIs(HunterView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    int health =0;
+    heath = currentView->playerStats[player]->health;
+    return health;
 }
 
 // Get the current location id of a given player
 LocationID whereIs(HunterView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    LocationID location = 0;
+    location = currentView->playerStats[player]->location;
+    return location;
 }
 
 //// Functions that return information about the history of the game
